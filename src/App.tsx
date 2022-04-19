1b0b58d2191bc7e7106578ac5909136751e17f23
Ali Ahmad <aliahmad5015@gmail.com>
2022-04-20 02:18:12 +0500

    All Done

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ function App() {
     <div className={classes.mainBody}>
       <Router>
         <Navbar />
+
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/product/:prodId" element={<DetailsPage />} />
